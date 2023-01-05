8e1f03ad8206f61af894a0741188f9c8d76bcacf
rohitinfolsom@gmail.com <rohitinfolsom@gmail.com>
2023-01-05 00:25:03 +0000

    Commit#2

--- models/example/EDW/test1.sql
+++ models/example/EDW/test1.sql
@@ -1,2 +1,6 @@
-select * from INFORMATION_SCHEMA.tables where table_Catalog = 'DBT';
-
+with 
+DBT_TABS as (
+select * from DBT.INFORMATION_SCHEMA.tables where table_Catalog = 'DBT'),
+DBT_COLUMNS as (
+select * from DBT.INFORMATION_SCHEMA.columns where table_Catalog = 'DBT')
+select * from DBT_TABS
