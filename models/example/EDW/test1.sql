b19e0f3e2b5e6290ac42dca6992b6e2c02f5307f
rohitinfolsom@gmail.com <rohitinfolsom@gmail.com>
2023-01-05 03:47:21 +0000

    dd

--- models/example/EDW/test1.sql
+++ models/example/EDW/test1.sql
@@ -3,6 +3,6 @@ DBT_TABS as (
 select * from DBT.INFORMATION_SCHEMA.tables where table_Catalog = 'DBT'),
 DBT_COLUMNS as (
 select * from DBT.INFORMATION_SCHEMA.columns where table_Catalog = 'DBT')
-select * from DBT_TABS
+select * from DBT_TABS;
 
 
