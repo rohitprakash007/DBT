091e3497cea32975a11e6fc14b4b4fff3664e9fb
rohitinfolsom@gmail.com <rohitinfolsom@gmail.com>
2023-01-09 20:40:25 +0000

    hh

--- models/example/EDW/test1.sql
+++ models/example/EDW/test1.sql
@@ -1,9 +1,13 @@
 with
     dbt_tabs as (
-        select * from dbt.information_schema.tables where table_catalog = 'DBT'
+        select *
+        from {{ source("information_schema", "tables") }}
+        where table_catalog = 'DBT'
     ),
     dbt_columns as (
-        select * from dbt.information_schema.columns where table_catalog = 'DBT'
+        select *
+        from {{ ("information_schema", "columns") }}
+        where table_catalog = 'DBT'
     )
 select *
 from dbt_tabs
